fdf3d1bb89459301508818e985df03da70d0f554
jim <ajammyfish@proton.me>
2023-06-15 18:29:53 +0100

    fixed unescaped chars

--- src/app/about/bespokesoftware/page.jsx
+++ src/app/about/bespokesoftware/page.jsx
@@ -37,9 +37,9 @@ const BespokeSoftware = () => {
                 <ul>
                     <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>Full-Stack Web Applications:</span> Customised web application development tailored to your specific business needs.</li>
                     <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>Glide Apps (No-Code Platform):</span> Rapid, trustworthy, affordable development of custom mobile and web app solutions.</li>
-                    <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>User friendly interfaces:</span> Designed with the user experience in mind, I'll build elegant apps that flow from start to finish.</li>
+                    <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>User friendly interfaces:</span> Designed with the user experience in mind, I&apos;ll build elegant apps that flow from start to finish.</li>
                     <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>Scalable and Robust Architecture:</span> Built to last and handle increasing user demands.</li>
-                    <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>Continuous support and maintenance:</span> I'll continue to maintain and update your application to ensure optimal performance.</li>
+                    <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>Continuous support and maintenance:</span> I&apos;ll continue to maintain and update your application to ensure optimal performance.</li>
                 </ul>
             </div>
         </motion.div>
@@ -51,7 +51,7 @@ const BespokeSoftware = () => {
                     <li>Cost-effective development and faster time-to-market compared to traditional coding</li>
                     <li>Seamless integration with various data sources</li>
                     <li>Real-time collaboration and data synchronization across devices and platforms</li>
-                    <li>Ongoing updates and optimizations leveraging Glide's platform advancements</li>
+                    <li>Ongoing updates and optimizations leveraging Glide&apos;s platform advancements</li>
                     </ul>} />
                 <Price price={49} title={'Full-Stack Web Apps'} description={<ul>
                     <li>Development tailored to your specific business needs</li>
--- src/app/about/webdesign/page.jsx
+++ src/app/about/webdesign/page.jsx
@@ -36,10 +36,10 @@ const WebDesign = () => {
             <div className="wd-points">
                 <ul>
                     <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>Elegant Website Design:</span> Captivate your audience with beautifully crafted, modern websites that load fast, and leave a lasting impression.</li>
-                    <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>Search Engine Optimisation:</span> By implementing the most up to date on-page best SEO practices, I'll increase your websites ranking on search engines, driving more traffic to your business.</li>
-                    <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>Deep Customisation:</span> If you've got ideas, I want to hear them! I create deeply customisable, bespoke designs taylored to your industry and preferences.</li>
-                    <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>Ongoing Support and Maintenance:</span> I will continue to be of service to you after designing and deploying your website. If you wish to update the site in any way simply let me know and I'll get it done.</li>
-                    <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>User Statistics Reports:</span> Each quarter, I'll generate you a report of the websites traffic and what users are searching for to find your site, alowing you to see the progression of your online presence first hand.</li>
+                    <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>Search Engine Optimisation:</span> By implementing the most up to date SEO practices, I&apos;ll increase your websites ranking on search engines, driving more traffic to your business.</li>
+                    <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>Deep Customisation:</span> If you&apos;ve got ideas, I want to hear them! I create deeply customisable, bespoke designs tailored to your industry and preferences.</li>
+                    <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>Ongoing Support and Maintenance:</span> I&apos;ll continue to be of service to you after designing and deploying your website. If you wish to update the site in any way simply let me know and I&apos;ll get it done.</li>
+                    <li><span style={{color: 'blueviolet', fontWeight: 'bold', textDecoration: 'underline'}}>User Statistics Reports:</span> Each quarter, I&apos;ll generate you a report of the websites traffic and what users are searching for to find your site, alowing you to see the progression of your online presence first hand.</li>
                 </ul>
             </div>
         </motion.div>
@@ -47,7 +47,7 @@ const WebDesign = () => {
         <div className="wd-prices">
             <h1 className="wdp-header">Pricing</h1>
             <div className="price-cards">
-                <Price price={199} title={'Starter Tier'} description={<ul>
+                <Price price={249} title={'Starter Tier'} description={<ul>
                     <li>Bespoke website design up to 5 pages</li>
                     <li>Responsive design for cross device compatibility</li>
                     <li>Custom domain integration</li>
@@ -56,7 +56,7 @@ const WebDesign = () => {
                     <li>Social media integration</li>
                     <li>Basic content population</li>
                     </ul>} />
-                <Price price={299} title={'Advanced Tier'} description={<ul>
+                <Price price={349} title={'Advanced Tier'} description={<ul>
                     <li>All features included in the Starter Tiers</li>
                     <li>Bespoke website design up to 10 pages</li>
                     <li>Enhanced design customisation</li>
@@ -64,7 +64,7 @@ const WebDesign = () => {
                     <li>Content population assistance</li>
                     <li>Basic website analytics setup</li>
                     </ul>} />
-                <Price price={399} title={'Pro Tier'} description={<ul>
+                <Price price={499} title={'Pro Tier'} description={<ul>
                     <li>All features included in the Starter and Advanced Tiers</li>
                     <li>Up to 15 pages</li>
                     <li>Payments Integration</li>
